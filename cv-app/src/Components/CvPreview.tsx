2b67fc8a31f5743af52ea69daf0870dbd07cc075
SalahAmm <salahamm4year@gmail.com>
2025-08-18 15:11:52 +0100

    Add small Functions

---
+++ cv-app/src/Components/CvPreview.tsx
@@ -0,0 +1,7 @@
+export default function CvPreview () {
+
+    return (
+        <>
+        </>
+    )
+}
---
+++ cv-app/src/Components/GeneralInfo.tsx
@@ -0,0 +1,15 @@
+import PersonalInfo from "./PersonalInfo.tsx"
+import EducationExp from "./EducationExp.tsx"
+import PracticalExp from "./PracticalExp.tsx"
+
+
+
+export default function GeneralInfo(){
+    return (
+        <>
+        <PersonalInfo />
+        <EducationExp /> 
+        <PracticalExp />
+        </>
+    )
+}
